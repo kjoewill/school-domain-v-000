5bc53322b0b14e8a2500cfba3274fb95b8688446
Kevin W <kjoewill@gmail.com>
2018-08-23 20:56:51 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,7 +8,7 @@ class School
   end
   
   def add_student(student_name, grade)
-    @roster[grade] = [] if @roster[grade] == nil
+    @roster[grade] ||= [] 
     @roster[grade] << student_name
   end
   
