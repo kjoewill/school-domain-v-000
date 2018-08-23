38f0d26eee943d0124f3bf8a2cd2dce854917856
Kevin W <kjoewill@gmail.com>
2018-08-23 20:50:58 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -18,4 +18,4 @@ school = School.new("Test School")
 school.add_student("AC Slater", 9)
 school.add_student("Kelly Kapowski", 10)
 school.add_student("Screech", 11)
-school.roster
+puts school.roster.inspect
