3d70283965c96431235722f64cd71e8bf46607d7
Kevin W <kjoewill@gmail.com>
2018-08-23 20:49:43 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,15 @@
-# code here!
+class School
+  
+  attr_reader :name, :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(student_name, grade)
+    @roster[grade] = [] if @roster[grade] == nil
+    @roster[grade] << student_name
+  end
+    
+end
